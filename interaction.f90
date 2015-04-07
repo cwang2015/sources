b5c6073bc0727ff31d075a54f94e78cc10013d24
cwang2015 <chunwang@sjtu.edu.cn>
2015-04-07 13:50:32 +0800

    renamed param.f as param.f90

--- interaction.f90
+++ interaction.f90
@@ -1,20 +1,5 @@
       subroutine darcy_law(water, soil)
 
-!--------------------------------------------------------------------------
-!     Subroutine to calculate the external forces, e.g. gravitational forces.      
-!     The forces from the interactions with boundary virtual particles 
-!     are also calculated here as external forces.
-
-!     here as the external force. 
-!     ntotal  : Number of particles                                 [in]
-!     mass    : Particle masses                                     [in]
-!     x       : Coordinates of all particles                        [in]
-!     pair_i : List of first partner of interaction pair            [in]
-!     pair_j : List of second partner of interaction pair           [in]
-!     itype   : type of particles                                   [in]
-!     hsml   : Smoothing Length                                     [in]
-!     dvxdt   : Acceleration with respect to x, y and z            [out] 
-
       use param, only: volume_fraction
       use m_particles
       implicit none
