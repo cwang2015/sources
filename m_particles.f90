8009c19a4b240328b9eef0f8aa71bdef1e09cf31
CWang <chunwang@sjtu.edu.cn>
2015-04-29 21:13:04 +0800

    m_particles modified

--- m_particles.f90
+++ m_particles.f90
@@ -5,16 +5,16 @@ use constants, ex_pi=>pi
 implicit none
 
 ! Configuration of the water-jet problem; Problem specific
-type geo
+!type geo
 
 ! size of each dimension
-  double precision dl,dr,dh,ds,dj,D,L,H,immerse
+!  double precision dl,dr,dh,ds,dj,D,L,H,immerse
 ! descrete intervals
-  integer  mdl,mdr,mdh,mds,mdj,mD,mL,mH
-  contains
-    procedure :: set => set_geo_sub
+!  integer  mdl,mdr,mdh,mds,mdj,mD,mL,mH
+!  contains
+!    procedure :: set => set_geo_sub
 
-end type
+!end type
 
 ! Rectangular area
 
@@ -30,25 +30,165 @@ type block
      procedure :: cell_center => get_cell_center_sub
 end type
 
-type zone
-     character(len=32) name
-     real(dp) phi_s, phi_f
-end type
+!---------------------------------------------------------
+type parameters
 
-type particle
-   integer itype
-   real(dp) x, y, vol, hsml, mass
-   contains
-      procedure :: copy => copy_particle_sub
-end type
+!Math parameters
+real(dp) :: pi = 3.14159265358979323846
 
-type enumerator
-   integer :: inside = 0, inlet = 1, boundary = 2
-end type
+!Physics parameter
+real(dp) :: gravity = -9.8
 
-type tensor
-   real(dp) xx,xy,xz,yy,yz,zz
-end type
+!Geometry parameters
+
+!dim : Dimension of the problem (1, 2 or 3)
+integer :: dim = 2
+
+!Parameters for the computational geometry,  
+!x_maxgeom : Upper limit of allowed x-regime 
+!x_mingeom : Lower limit of allowed x-regime 
+!y_maxgeom : Upper limit of allowed y-regime 
+!y_mingeom : Lower limit of allowed y-regime 
+!z_maxgeom : Upper limit of allowed z-regime 
+!z_mingeom : Lower limit of allowed z-regime 
+real(dp) :: x_maxgeom = 10.e0, x_mingeom = -10.e0,  &
+            y_maxgeom = 10.e0, y_mingeom = -10.e0,  &
+            z_maxgeom = 10.e0, z_mingeom = -10.e0
+
+!Memory allocation
+
+!maxn: Maximum number of particles
+!max_interation : Maximum number of interaction pairs
+integer :: maxn = 12000, max_interaction = 100 * 12000
+  
+!SPH algorithm
+
+!Particle approximation (pa_sph)
+!pa_sph = 1 : (e.g. (p(i)+p(j))/(rho(i)*rho(j))
+!         2 : (e.g. (p(i)/rho(i)**2+p(j)/rho(j)**2)
+integer :: pa_sph = 2 
+
+!Nearest neighbor particle searching (nnps) method
+!nnps = 1 : Simplest and direct searching
+!       2 : Sorting grid linked list
+!       3 : Tree algorithm
+integer :: nnps = 1 
+
+!Smoothing length evolution (sle) algorithm
+!sle = 0 : Keep unchanged,
+!      1 : h = fac * (m/rho)^(1/dim)
+!      2 : dh/dt = (-1/dim)*(h/rho)*(drho/dt)
+!      3 : Other approaches (e.g. h = h_0 * (rho_0/rho)**(1/dim) ) 
+integer :: sle = 0 
+
+!Smoothing kernel function 
+!skf = 1, cubic spline kernel by W4 - Spline (Monaghan 1985)
+!    = 2, Gauss kernel   (Gingold and Monaghan 1981) 
+!    = 3, Quintic kernel (Morris 1997)
+!    = 4, Wendland    
+integer :: skf = 4 
+
+!Switches for different senarios
+
+!summation_density = .TRUE. : Use density summation model in the code, 
+!                    .FALSE.: Use continuiity equation
+!average_velocity = .TRUE. : Monaghan treatment on average velocity,
+!                   .FALSE.: No average treatment.
+!config_input = .TRUE. : Load initial configuration data,
+!               .FALSE.: Generate initial configuration.
+!virtual_part = .TRUE. : Use vritual particle,
+!               .FALSE.: No use of vritual particle.
+!vp_input = .TRUE. : Load virtual particle information,
+!           .FALSE.: Generate virtual particle information.
+!visc = .true. : Consider viscosity,
+!       .false.: No viscosity.
+!ex_force =.true. : Consider external force,
+!          .false.: No external force.
+!visc_artificial = .true. : Consider artificial viscosity,
+!                  .false.: No considering of artificial viscosity.
+!heat_artificial = .true. : Consider artificial heating,
+!                  .false.: No considering of artificial heating.
+!self_gravity = .true. : Considering self_gravity,
+!               .false.: No considering of self_gravity
+!nor_density =  .true. : Density normalization by using CSPM,
+!               .false.: No normalization.
+
+integer :: integrate_scheme = 1  ! =1, LF; =2, Verlet
+logical :: summation_density  = .false.         
+logical :: average_velocity  = .true.         
+logical :: config_input  = .false. 
+logical :: virtual_part  = .true. 
+logical :: vp_input  = .false.  
+logical :: visc  = .true.  
+logical :: ex_force  = .true.
+logical :: visc_artificial  = .true. 
+logical :: heat_artificial  = .false. 
+logical :: self_gravity  = .true.      
+logical :: nor_density  = .false.              
+
+integer :: soil_pressure = 2  ! =1, eos; =2, mean trace
+integer :: stress_integration = 1
+integer :: yield_criterion = 2
+integer :: plasticity = 3  ! =0 non; =1 Bui, =2 return mapping =3 Lopez
+logical :: artificial_density = .true.                  
+logical :: soil_artificial_stress = .true.
+
+logical :: volume_fraction = .true.
+logical :: water_artificial_volume = .true.
+logical :: volume_fraction_renorm = .true.
+
+! 0 ignor; 1 negative pressure to zero; 2 artficial stress
+integer :: water_tension_instability = 0
+
+! Symmetry of the problem
+! nsym = 0 : no symmetry,
+!      = 1 : axis symmetry,
+!      = 2 : center symmetry.     
+integer :: nsym = 0
+
+! Control parameters for output 
+! int_stat = .true. : Print statistics about SPH particle interactions.
+!                     including virtual particle information.
+! print_step: Print Timestep (On Screen)
+! save_step : Save Timestep    (To Disk File)
+! moni_particle: The particle number for information monitoring.
+logical :: int_stat = .true.
+integer :: print_step, save_step, moni_particle = 264
+          
+!Simulation cases
+
+!shocktube = .true. : carry out shock tube simulation
+!shearcavity = .true. : carry out shear cavity simulation
+!logical shocktube, shearcavity, waterjet
+logical :: shocktube  = .false. 
+logical :: shearcavity  = .false. 
+logical :: waterjet = .true.
+
+!Recorde time interval
+integer :: save_step_from = 0, save_step_to = 100
+
+!Material parameters
+
+!For equation of state (EOS) of water
+
+!real(dp) rho0_f,b,gamma,cf,viscosity
+
+!For soil
+
+!real(dp) rho0_s,cs,k,porosity,permeability,G,E,niu,cohesion,phi
+   
+! Artificial viscosity
+! alpha: shear viscosity; beta: bulk viscosity; etq: to avoid sigularities   
+!real(dp) :: alpha=0.1d0, beta=0.d0, etq=0.1d0
+
+! Leonard_Johns repulsive force
+!real(dp) :: rr0 = 0.005d0, dd = 0.1d0, p1 = 12, p2 = 4
+
+! Delta-SPH
+!real(dp) :: delta = 0.1d0
+        
+end type parameters 
+!-----------------------------------------------------------------
 
 type material
 
@@ -105,6 +245,8 @@ type particles
    integer :: max_interaction = 0
    integer :: ntotal = 0, nvirt = 0
    integer :: max_zone = 10
+
+   class(parameters), pointer :: params => null()
 
    character(len=32) :: imaterial
    class(*), pointer :: material => null()
@@ -255,6 +397,7 @@ end interface
 contains
 !=======
 
+!DEC$IF(.FALSE.)
 !---------------------------------------------------------------------------------
    subroutine set_geo_sub(this,dl,dr,dh,ds,dj,D,L,H,immerse,       &
                               mdl,mdr,mdh,mds,mdj,mD,mL,mH)
@@ -286,6 +429,7 @@ contains
    return
    end subroutine
 
+!DEC$ENDIF
 !--------------------------------------------
    subroutine set_block_sub(this,xl,yl,m,n)
 !--------------------------------------------
@@ -325,16 +469,16 @@ return
 end subroutine
 
 !-------------------------------------------
-   subroutine copy_particle_sub(this,that)
+!   subroutine copy_particle_sub(this,that)
 !-------------------------------------------
-implicit none
-class(particle) this
-class(particle)  that
-
-this%x = that%x
-
-return
-end subroutine
+!implicit none
+!class(particle) this
+!class(particle)  that
+!
+!this%x = that%x
+!
+!return
+!end subroutine
 
 !----------------------------------------
    subroutine write_particles(this) 
